c663131328bfb80141c6e3185d54732be83b8afd
Hadeeqa Saman <hadeeqasaman95@gmail.com>
2019-11-01 10:58:51 -0400

    deployed

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ link.onclick= function(){
 span.onclick= function(){
     modal.style.display = "none";
 }
+
 window.onclick= function(event){
     if (event.target === modal){
         modal.style.display = "none";
     }
+
 }
